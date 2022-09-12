6789cf87860c38317189f40dd42908062bae48ef
Guillaume Garchery <polluxxx@gmail.com>
2022-09-12 16:13:12 +0200

    first coingecko download

---
+++ draft.py
@@ -0,0 +1,23 @@
+from pycoingecko import CoinGeckoAPI
+from datetime import datetime
+import numpy as np
+import pandas as pd
+
+
+cg = CoinGeckoAPI()
+
+ddata = cg.get_coin_market_chart_by_id(id = "bitcoin", vs_currency="USD", days=250)
+
+x = ddata["prices"][-1][0]
+
+datetime.fromtimestamp(x / 1000)
+
+dfs = []
+for k in ddata.keys():
+  dfs.append(pd.DataFrame(ddata["prices"], columns=["timestamp", k]).set_index("timestamp"))
+
+df = pd.concat(dfs, axis=1)
+df["id"] = "bitcoin"
+df["numeraire"] = "usd"
+df
+
